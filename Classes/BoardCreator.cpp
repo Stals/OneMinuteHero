9c14ad45c1e593d1fb8c2019ac19c4ac78e2e0a1
Stals <superstals@gmail.com>
2013-12-15 18:11:42 +0400

    load maps from files

--- Classes/BoardCreator.cpp
+++ Classes/BoardCreator.cpp
@@ -4,30 +4,49 @@
 #include "EmptyTile.h"
 #include "Stairs.h"
 #include <algorithm>
+#include <fstream>
+#include "StringExtension.h"
+
+#define LEVELS 5
 
 void BoardCreator::create(std::vector<std::vector<BoardTile*> >& tiles)
 {
 	int width = 15;
 	int height = 18;
 
-	tiles.resize(width);
+	std::string filename = "levels/";
+	filename.append(StringExtension::toString(rand()%LEVELS + 1)).append(".txt");
 
+	std::ifstream infile(filename.c_str());
+
+	std::vector<std::string> lines;
+	std::string line;
+	while (std::getline( infile, line))
+	{
+		lines.push_back(line);
+		std::cout<<line<<std::endl;
+	}
+
+	/***/
+	tiles.resize(width);
 	for(int x = 0; x < width; ++x){
 		tiles[x].resize(height);
 		for(int y = 0; y < height; ++y){
-			
-			int r = rand() % 5;
-			
-			if(r == 0){
-				BoardTile* tile = new Wall;
-				tiles[x][y] = tile;
+			char tileChar = lines[y][x];
+			BoardTile* tile = NULL;
 
-			}else{
-				BoardTile* tile = new EmptyTile;
-				tiles[x][y] = tile;
+			switch(tileChar){
+			case '#':
+				tile = new Wall;
+				break;
+			case ' ':
+			default:
+				tile = new EmptyTile;
+				break;
 			}
+
+			tiles[x][y] = tile;
 		}
 	}
-
 }
 
