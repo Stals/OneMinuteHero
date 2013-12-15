b84b62293a39ebe269d14ff5a7f5f12ee574028b
Stals <superstals@gmail.com>
2013-12-15 17:27:12 +0400

    spawn monsters with random sprite changed ghost color

--- Classes/Monster.cpp
+++ Classes/Monster.cpp
@@ -1,6 +1,8 @@
 #include "Monster.h"
+#include "StringExtension.h"
 
-Monster::Monster(int maxHp, int damage): Creature("monster.png", maxHp, damage){
+Monster::Monster(int maxHp, int damage): 
+Creature((std::string("monster") + StringExtension::toString(rand()%5 +1) + ".png").c_str(), maxHp, damage){
 	/*CCSprite* sprite = CCSprite::create("monster.png");
 	this->setContentSize(sprite->getContentSize());
 	this->setAnchorPoint(ccp(0,0));
